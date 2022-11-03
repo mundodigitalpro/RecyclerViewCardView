a1228e06039d1910a6ef421f354ede7a4a962b12
JordanPro <mundodigital.pro@gmail.com>
2022-11-03 14:05:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewcardview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.recyclerviewcardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<ListElement> elements;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init(){
+        elements = new ArrayList<>();
+        elements.add(new ListElement("#775447", "Pedro", "Mexico", "Activo"));
+        elements.add(new ListElement("#607D8B", "Julio", "Tabasco", "Activo"));
+        elements.add(new ListElement("#03A9F4", "Alejandra", "Chihuahua", "Cancelado"));
+        elements.add(new ListElement("#F44336", "Jessica", "Durango", "Inactivo"));
+        elements.add(new ListElement("#009688", "Armando", "Yucatan", "Activo"));
+
+        ListAdapter listAdapter = new ListAdapter(elements, this);
+        RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
+    }
+}
